ec0aee9d9b4d82025e175548da03dc2d47e65d7f
Moarten <sailor1991@msn.com>
2015-05-12 14:17:12 +0200

    -FFS in notepad++ i cant see any difference between 1 tab or 4 whitespaces, but for some reason the python vm can and wants to kill me for it.

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ con = mdb.connect('localhost','koelkast','amstelbier','koelkast');
 
 def radioSetup():
     pipes = [[0xf0, 0xf0, 0xf0, 0xf0, 0xe1], [0xf0, 0xf0, 0xf0, 0xf0, 0xd2]]
-	radio = NRF24()
-	radio.begin(0, 0,25,18)
-	radio.setRetries(15,15)
-	radio.setPayloadSize(32)
-	radio.setChannel(0x4c)
-	radio.setDataRate(NRF24.BR_250KBPS)
-	radio.setPALevel(NRF24.PA_MAX)
-	radio.openWritingPipe(pipes[0])
-	radio.openReadingPipe(1, pipes[1])
-	radio.startListening()
-	radio.stopListening()
-	radio.write("*OK#")
-	radio.printDetails()
-	radio.startListening()
+    radio = NRF24()
+    radio.begin(0, 0,25,18)
+    radio.setRetries(15,15)
+    radio.setPayloadSize(32)
+    radio.setChannel(0x4c)
+    radio.setDataRate(NRF24.BR_250KBPS)
+    radio.setPALevel(NRF24.PA_MAX)
+    radio.openWritingPipe(pipes[0])
+    radio.openReadingPipe(1, pipes[1])
+    radio.startListening()
+    radio.stopListening()
+    radio.write("*OK#")
+    radio.printDetails()
+    radio.startListening()
 	return
 	
 def explodeString(received):
