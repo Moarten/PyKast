0f2daeeaf8e83c9f350211320d670484eedcccef
Moarten <sailor1991@msn.com>
2015-05-12 13:57:54 +0200

    -Moved radio setup to seperate file

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 from nrf24 import NRF24
 import MySQLdb as mdb
-import sys
+import sys, nrf24_setup
 import time
 from time import gmtime, strftime
-#NRF24 stuff
-pipes = [[0xf0, 0xf0, 0xf0, 0xf0, 0xe1], [0xf0, 0xf0, 0xf0, 0xf0, 0xd2]]
-
-radio = NRF24()
-radio.begin(0, 0,25,18)
-radio.setRetries(15,15)
-radio.setPayloadSize(32)
-radio.setChannel(0x4c)
-radio.setDataRate(NRF24.BR_250KBPS)
-radio.setPALevel(NRF24.PA_MAX)
-radio.openWritingPipe(pipes[0])
-radio.openReadingPipe(1, pipes[1])
-radio.startListening()
-radio.stopListening()
-radio.write("*OK#")
-radio.printDetails()
-radio.startListening()
-charsToRemove = ['*','#']
-sensors = {'T': 0, 'H': 1}
 
 #DB stuff
 con = mdb.connect('localhost','koelkast','amstelbier','koelkast');
---
+++ nrf24_setup.py
@@ -0,0 +1,19 @@
+#NRF24 stuff
+pipes = [[0xf0, 0xf0, 0xf0, 0xf0, 0xe1], [0xf0, 0xf0, 0xf0, 0xf0, 0xd2]]
+
+radio = NRF24()
+radio.begin(0, 0,25,18)
+radio.setRetries(15,15)
+radio.setPayloadSize(32)
+radio.setChannel(0x4c)
+radio.setDataRate(NRF24.BR_250KBPS)
+radio.setPALevel(NRF24.PA_MAX)
+radio.openWritingPipe(pipes[0])
+radio.openReadingPipe(1, pipes[1])
+radio.startListening()
+radio.stopListening()
+radio.write("*OK#")
+radio.printDetails()
+radio.startListening()
+charsToRemove = ['*','#']
+sensors = {'T': 0, 'H': 1}
